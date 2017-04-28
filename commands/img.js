926dda2aa2db86baca7e193e208e0fdac4546815
EPICZEUS <sbepiczeus@aol.com>
2017-04-28 02:56:02 -0400

    Added dir not found handling

--- commands/img.js
+++ commands/img.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 
 exports.run = (bot, message, args) => {
+	if (!fs.existsSync("./images/")) {
+		try {
+			fs.mkdirSync("./images/");
+			
+			return console.log("The directory images/ did not exist, so it was created.\nPut images in there to be able to send them.");
+		} catch (err) {
+			console.error(err);
+		}
+	}
 	fs.readdir("./images", (err, files) => {
 		if (err) return console.error(err);
 
--- commands/randimg.js
+++ commands/randimg.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
 
 exports.run = (bot, message, args) => {
+	if (!fs.existsSync("./images/rand")) {
+		try {
+			fs.mkdirSync("./images/rand");
+
+			return console.log("The directory images/rand did not exist, so it was created.\nPut images in there to be able to send them.");
+		} catch (err) {
+			console.error(err);
+		}
+	}
 	let files = fs.readdirSync("./images/rand");
 
-	if (args.length) files = files.filter(a => a.startsWith(args.join("")));
+	if (args.length) files = files.find(a => a.startsWith(args.join("")));
 
 	if (!files.length) return console.log("Specified tag not found.");
 	
